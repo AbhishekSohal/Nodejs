82b73ab629bc375a464e6a843054321f6dcbab54
AbhishekSohal <abhisheksohal81@gmail.com>
2026-04-03 21:25:09 +0530

    http methods

--- http_server/index.js
+++ http_server/index.js
@@ -1,9 +1,12 @@
 const http=require('http');
 const fs=require("fs");
+const url=require("url")
 //creating server
 const myserver=http.createServer((req,res)=>{
     //console.log(req);// idhr ham keh rhe hain ki jab request aati hai to console log to krdo also request ko end krdo with a response
     const log=`${Date.now()}: new request recieved`;
+    const myurl=url.parse(req.url)
+    console.log(myurl)
     fs.appendFile('log.txt',log,(error,data)=>{
         res.end("hello from server")});
     //res.end("hello from server")
