742d85328b24ca7d27d9bfe103de72a3d67eac7a
AbhishekSohal <abhisheksohal81@gmail.com>
2026-04-19 00:03:20 +0530

    no changes

--- Model_views_Controller/models/user.js
+++ Model_views_Controller/models/user.js
@@ -22,5 +22,5 @@ const userSchema=new mongoose.Schema({
 },{timestamps:true}// it will add createdAt and updatedAt fields to the document
 )
 
-const User=mongoose.model('user',userSchema)// it will create a collection named users in the database and it will use the userSchema to create the documents in the collection it takes two arguments first is the name of the collection and second is the schema which we want to use to create the documents in the collection
+const User=mongoose.model('user',userSchema)// it will create a collection named user in the database and it will use the userSchema to create the documents in the collection it takes two arguments first is the name of the collection and second is the schema which we want to use to create the documents in the collection
 module.exports=User
