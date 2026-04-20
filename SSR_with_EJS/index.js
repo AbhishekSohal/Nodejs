3740c0d905f0f0d856960a83cf3c95abe47dba8a
AbhishekSohal <abhisheksohal81@gmail.com>
2026-04-21 00:13:26 +0530

    learnt server side rendering with ejs

---
+++ SSR_with_EJS/index.js
@@ -0,0 +1,25 @@
+const express=require('express');
+const path=require('path')
+const connectDB=require('./connection')
+const urlRoute=require('./routes/url')
+const staticRoute=require('./routes/staticRouter')
+const URL=require('./models/url')
+const app=express();
+const port=8003
+
+connectDB('mongodb://127.0.0.1:27017/short-url').then(()=>console.log('MongoDB connected')).catch(()=>console.log('coudnt connect try again'));
+
+app.set('view engine','ejs')
+app.set('views',path.resolve('./views'))
+
+ 
+
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}))
+app.use('/url',urlRoute)
+
+app.use('/',staticRoute)
+
+
+app.listen(port,()=>console.log('Server Started'))
