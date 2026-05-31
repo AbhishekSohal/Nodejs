9dee181e231ed07446edd4e0864ae2d40399706e
AbhishekSohal <abhisheksohal81@gmail.com>
2026-05-31 17:40:54 +0530

    storing session in database why not

--- JWT_Auth/Service/auth.js
+++ JWT_Auth/Service/auth.js
@@ -1,3 +1,5 @@
+// in doubt can come is that why in statefull we cannot store cookies in database itself because of security issues and also because of performance issues as we have to query the database every time to check if the cookie is valid or not but in stateless we can store the token in the client side and we can verify the token on the server side without having to query the database every time.
+
 
 const jwt = require('jsonwebtoken')
 const secret = '12345'
