687ab4325e58987135dd67a9ab8cc9dece36c4a7
AbhishekSohal <abhisheksohal81@gmail.com>
2026-08-11 10:16:22 +0530

    .

--- GraphQL/server/index.js
+++ GraphQL/server/index.js
@@ -1,17 +1,68 @@
 const express = require('express');
 const { ApolloServer } = require('@apollo/server');
-const { expressMiddleware } = require('@apollo/server/express4');
-const bodyParser = require('body-parser');
+const { expressMiddleware } = require('@as-integrations/express5');
 const cors = require('cors');
+const axios = require('axios');
 
-async function startApolloServer(typeDefs, resolvers) {
+const typeDefs = `
+  type User {
+    id: ID!
+    name: String!
+    email: String!
+  }
+  type Todo {
+    id: ID!
+    title: String!
+    completed: Boolean!
+    user: User!
+  }
+
+  type Query {
+    getTodos: [Todo!]!
+    getUsers: [User!]!
+    getUser(id: ID!): User // Fetch a single user by ID
+  }
+`;
+
+
+
+const resolvers = {
+    Todo: {
+        user: async (parent) => {
+            const response = await axios.get(`https://jsonplaceholder.typicode.com/users/${parent.userId}`);
+            return response.data;
+        }
+    },
+  Query: {
+    getTodos: async () => (await axios.get('https://jsonplaceholder.typicode.com/todos')).data,
+    getUsers: async () => (await axios.get('https://jsonplaceholder.typicode.com/users')).data,
+    getUser: async (_, { id }) => {
+      try {
+        const response = await axios.get(`https://jsonplaceholder.typicode.com/users/${id}`);
+        return response.data;
+      } catch (error) {
+        console.error(error);
+        return null; // Return null if user not found or an error occurs
+      }
+  },
+}};
+
+async function startApolloServer() {
   const app = express();
-  const server = new ApolloServer({});
-  app.use(bodyParser.json());
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+  });
   app.use(cors());
-  await server.start()
+  app.use(express.json());
+  await server.start();
   app.use('/graphql', expressMiddleware(server));
   app.listen({ port: 4000 }, () => {
     console.log(`🚀 Server ready at http://localhost:4000/graphql`);
   });
 }
+
+startApolloServer().catch((error) => {
+  console.error(error);
+  process.exit(1);
+});
